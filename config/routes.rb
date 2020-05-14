1619b8d4441f6bdff3de659369f18b9bc2efb77c
Tien Nguyen <tien.nguyen@reddoorz.com>
2020-05-14 17:53:03 +0700

    fix crashes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/reset", to: "token_transfers#reset"
-  post "/transfer", to: "token_transfers#transfer"
-  get "/get_amounts", to: "token_transfers#amount"
+  get "/reset", to: "token_transfer#reset"
+  post "/transfer", to: "token_transfer#transfer"
+  get "/get_amounts", to: "token_transfer#amount"
 end
