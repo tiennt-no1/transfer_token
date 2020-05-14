c5b6dcc6a07c88a2ffb83d0204608500e9b60ed0
Tien Nguyen <tien.nguyen@reddoorz.com>
2020-05-14 17:24:07 +0700

    update routest

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/reset", to: "TokenTransferController#reset"
+  post "/transfer", to: "TokenTransferController#transfer"
+  get "/get_amounts", to: "TokenTransferController#amount"
 end
