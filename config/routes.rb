a2e3d7e14e622a96316bce03938fd1aabbf5e682
Tien Nguyen <tien.nguyen@reddoorz.com>
2020-05-14 17:26:56 +0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/reset", to: "TokenTransferController#reset"
-  post "/transfer", to: "TokenTransferController#transfer"
-  get "/get_amounts", to: "TokenTransferController#amount"
+  get "/reset", to: "token_transfers#reset"
+  post "/transfer", to: "token_transfers#transfer"
+  get "/get_amounts", to: "token_transfers#amount"
 end
