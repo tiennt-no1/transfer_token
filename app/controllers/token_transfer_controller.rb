6175f174b9f86e764b830e7a03696bff642b90dc
Tien Nguyen <tien.nguyen@reddoorz.com>
2020-05-14 17:49:14 +0700

    add token to user to get current user

--- app/controllers/token_transfer_controller.rb
+++ app/controllers/token_transfer_controller.rb
@@ -1,37 +1,45 @@
 class TokenTransferController < ApplicationController
+  before_action :validate, only: :amount
 
-    def reset
-        User.all.each do |user|
-            user.amount = 1000
-            user.save
-        end
-        render json: {success: true}
+  def reset
+    User.all.each do |user|
+      user.amount = 1000
+      user.save
+    end
+    render json: { success: true }
+
+  end
+
+  def transfer
+    from_user = User.find_by_id params[:transfer_from]
+    to_user = User.find_by_id params[:transfer_to]
+
+    amount = params[:amount].to_f
+
+    if from_user.nil? || to_user.nil?
+    elsif amount > from_user.amount
+      raise "not enough amount to transfer"
+    else
+      from_user.amount -= amount
+      to_user.amount += amount
+      from_user.save
+      to_user.save
+      render json: { success: true, amount: amount }
 
     end
 
-    def transfer
-        from_user = User.find_by_id params[:transfer_from]
-        to_user = User.find_by_id params[:transfer_to]
 
-        amount = params[:amount].to_f
+  end
 
-        if from_user.nil? || to_user.nil?
-        elsif amount > from_user.amount
-        else
-            from_user.amount -= amount
-            to_user.amount += amount
-            from_user.save
-            to_user.save
-            render json: {success: true, amount: amount}
+  def amount
+    render json: current_user.amount
+  end
 
-        end
+  private
+
+  def current_user
+    User.validate(params[:token])
+  end
 
 
-    end
-
-    def amount
-        render json: current_user.amount
-    end
-
-    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,30 +1,44 @@
-require 'bcrypt'
 
 class User
 
+  attr_accessor :password, :id, :amount, :file_name, :token
 
-    attr_accessor :password, :id, :amount, :file_name
-
-    def initialize(hash)
-        hash.each do |key,val|
-            eval("@#{key} = #{val}")
-        end
+  def initialize(hash)
+    hash.each do |key, val|
+      eval("@#{key} = #{val}")
     end
+  end
 
-    def self.all
-        Dir['db/user*.txt'].each do |file|
-            data = JSON.parse(File.read(file))
-            user = User.new(data)
-            user.file_name = file
-        end
+  def self.all
+    Dir['db/user*.txt'].map do |file|
+      data = JSON.parse(File.read(file))
+      user = User.new(data)
+      user.file_name = file
+      user
     end
+  end
 
-    def to_hash
-        {id : @id, password: @password, amount: @amount}
-    end
+  def to_hash
+    { id: @id, password: @password, amount: @amount }
+  end
 
-    def save
-        File.write(@file_name, self.to_hash)
+  def save
+    File.write(@file_name, self.to_hash)
+  end
+
+  def self.find_by_id(id)
+    file = "user#{id}.txt"
+    data = JSON.parse(File.read(file))
+    user = User.new(data)
+    user.file_name = file
+    user
+  end
+
+  def validate(token)
+    User.all.each do |user|
+      return user if user.token == token
     end
+    nil
+  end
 
 end
