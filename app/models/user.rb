f052a7494c4052829d1eb3d3c520966dbe0f3b16
Tien Nguyen <tien.nguyen@reddoorz.com>
2020-05-14 17:21:02 +0700

    fix crash, create method User.all

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 require 'bcrypt'
 
 class User
-    attr_accessor :name, :password
+
+
+    attr_accessor :password, :id, :amount
+
+    def initialize(hash)
+        hash.each do |key,val|
+            eval("@#{key} = #{val}")
+        end
+    end
+
+    def self.all
+        Dir['db/user*.txt'].each do |file|
+            data = JSON.parse(File.read(file))
+            user = User.new(data)
+            pp user
+
+        end
+    end
 
 end
